c397471d4660c837834b752af4b6739b067c8c97
fred <abnertorres58@gmail.com>
2022-05-09 16:57:16 -0400

    Angular Project - Update Angular Component

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from "../../services/product.service";
+import { Product } from "../../common/product";
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  // @ts-ignore
+  products: Product[];
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+
+    this.listProducts();
+  }
+
+  listProducts() {
+
+    // Method is invoked once you "subscribe"
+    this.productService.getProductList().subscribe(
+      data => {
+        this.products = data; // Assign results to the Product array
+      }
+    )
   }
 
 }

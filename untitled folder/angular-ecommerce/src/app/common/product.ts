9d432e4694593c80e0ad8b4d31dae2c69ff7a36d
fred <abnertorres58@gmail.com>
2022-05-09 21:26:55 -0400

    Angular Project - Integrate Template Files and Logos

---
+++ untitled folder/angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,20 @@
+export class Product {
+  // @ts-ignore
+  sku: string;
+  // @ts-ignore
+  name: string;
+  // @ts-ignore
+  description: string;
+  // @ts-ignore
+  unitPrice: number;
+  // @ts-ignore
+  imageUrl: string;
+  // @ts-ignore
+  active: boolean;
+  // @ts-ignore
+  unitsInStock: number;
+  // @ts-ignore
+  dateCreated: Date;
+  // @ts-ignore
+  lastUpdate: Date;
+}
--- untitled folder/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ untitled folder/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,11 +1,10 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 import { ProductService } from "../../services/product.service";
 import { Product } from "../../common/product";
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list-grid.component.html',
-  // templateUrl: './product-list-table.component.html',
+  templateUrl: './product-list-table.component.html',
   // templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })

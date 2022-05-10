ee1125676036d8efff5d21b08f441439edc92b2c
fred <abnertorres58@gmail.com>
2022-05-10 18:49:16 -0400

    Angular Project - Search for Products by Category - Code - Define routes

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -5,6 +5,15 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import {HttpClientModule} from "@angular/common/http";
 import {ProductService} from "./services/product.service";
+import {RouterModule, Routes} from "@angular/router";
+
+const routes: Routes = [
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
@@ -12,6 +21,7 @@ import {ProductService} from "./services/product.service";
     ProductListComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule
   ],

df3197e5daefd4f2d754322fd2a3cf3818e83f26
fred <abnertorres58@gmail.com>
2022-05-10 23:37:54 -0400

    Angular Project - Dynamic Search Component - Modify Spring Boot Appt

--- 02-backend/spring-boot-ecomerce/src/main/java/com/freudromero/ecomerce/config/MyDataRestConfig.java
+++ 02-backend/spring-boot-ecomerce/src/main/java/com/freudromero/ecomerce/config/MyDataRestConfig.java
@@ -2,14 +2,29 @@ package com.freudromero.ecomerce.config;
 
 import com.freudromero.ecomerce.entity.Product;
 import com.freudromero.ecomerce.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager theEntityManager) {
+        this.entityManager = theEntityManager;
+    }
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
@@ -27,5 +42,30 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        // Call an internal helper method
+        exposeIds(config);
+        
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+
+        // Expose entity ids
+
+        // - Get a list of all entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        // - Create an array of the entity types
+        List<Class> entityClasses = new ArrayList<>();
+
+        // - Get the entity types for the entities
+        for (EntityType tempEntityType : entities) {
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        // - Expose the entity ids for the array of entity/domain types
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
+
     }
 }

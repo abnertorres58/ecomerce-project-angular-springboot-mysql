40defca26ff437080b9d8928271c13bf43fc1398
fred <abnertorres58@gmail.com>
2022-05-10 22:56:58 -0400

    Angular Project - Search for Products by Category - Code - Update SpringBoot

--- 02-backend/spring-boot-ecomerce/src/main/java/com/freudromero/ecomerce/entity/ProductCategory.java
+++ 02-backend/spring-boot-ecomerce/src/main/java/com/freudromero/ecomerce/entity/ProductCategory.java
@@ -17,7 +17,7 @@ public class ProductCategory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private Long Id;
+    private Long id;
 
     @Column(name = "category_name")
     private String categoryName;

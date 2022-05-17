89d84be6e5041bfef1781441b197a623fa25ce09
fred <abnertorres58@gmail.com>
2022-05-17 15:53:53 -0400

    Checkout Form - Populate Countries and States - Repositories Improved

---
+++ 02-backend/spring-boot-ecomerce/src/main/java/com/freudromero/ecomerce/dao/StateRepository.java
@@ -0,0 +1,17 @@
+package com.freudromero.ecomerce.dao;
+
+import com.freudromero.ecomerce.entity.State;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
+
+@CrossOrigin("http//localhost:4200")
+@RepositoryRestResource
+public interface StateRepository  extends JpaRepository<State, Integer> {
+
+    List<State> findByCountryCode(@Param("code") String code);
+
+}

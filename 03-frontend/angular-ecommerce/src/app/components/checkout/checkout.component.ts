147af4e20b7c94c3b889d3f33c1197e0a6c28b89
fred <abnertorres58@gmail.com>
2022-05-17 23:08:05 -0400

    Angular Project - Checkout Form - Custom Validator Rule - Define the Rule

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -3,6 +3,7 @@ import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {ShopFormService} from "../../services/shop-form.service";
 import {Country} from "../../common/country";
 import {State} from "../../common/state";
+import {ShopValidators} from "../../validators/shop-validators";
 
 @Component({
   selector: 'app-checkout',
@@ -32,8 +33,13 @@ export class CheckoutComponent implements OnInit {
 
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
-        lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        firstName: new FormControl('', [ Validators.required,
+                                                              Validators.minLength(2),
+                                                              ShopValidators.notOnlyWhiteSpace]),
+
+        lastName: new FormControl('', [ Validators.required,
+                                                              Validators.minLength(2),
+                                                              ShopValidators.notOnlyWhiteSpace]),
         email: new FormControl('',
           [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),

d735b32664c60672bb18a6c7ac3dd5704607a76d
fred <abnertorres58@gmail.com>
2022-05-20 23:02:23 -0400

    Commerce Project - Checkout Form - Save the Order to Database - REST Controller

---
+++ 02-backend/spring-boot-ecomerce/src/main/java/com/freudromero/ecomerce/controller/CheckoutController.java
@@ -0,0 +1,28 @@
+package com.freudromero.ecomerce.controller;
+
+import com.freudromero.ecomerce.dto.Purchase;
+import com.freudromero.ecomerce.dto.PurchaseResponse;
+import com.freudromero.ecomerce.service.CheckoutService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+
+    private CheckoutService checkoutService;
+
+    @Autowired
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+
+        return purchaseResponse;
+    }
+}

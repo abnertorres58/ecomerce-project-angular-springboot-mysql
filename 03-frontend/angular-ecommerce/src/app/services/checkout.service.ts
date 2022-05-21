ceb7cb5d41b645ec15cd38cdca8415fc6451dd35
fred <abnertorres58@gmail.com>
2022-05-20 23:26:17 -0400

    Checkout Form - Save the Order - Frontend - Checkout Service

---
+++ 03-frontend/angular-ecommerce/src/app/services/checkout.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Purchase} from "../common/purchase";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CheckoutService {
+
+  private purchaseUrl = 'http://localhost:8080/api/checkout/purchase';
+
+  constructor(private httpClient: HttpClient) { }
+
+  placeOrder(purchase: Purchase): Observable<any> {
+    return this.httpClient.post<Purchase>(this.purchaseUrl, purchase);
+  }
+}

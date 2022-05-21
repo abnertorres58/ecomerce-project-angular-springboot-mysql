fdf4c8e757f77f6a86398dce051e6dde19f26eaf
fred <abnertorres58@gmail.com>
2022-05-21 02:49:38 -0400

    Security - Login/Logout - Coding - Develop App Configuration

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+  oidc: {
+    clientId: '0oa54mwi8xcsnqfMk5d7',
+    issuer: 'https://dev-74646655.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email']
+  }
+
+}

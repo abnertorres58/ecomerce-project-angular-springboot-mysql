25f934e83bb9c3989f00ec2dccfcb896020fd227
fred <abnertorres58@gmail.com>
2022-05-26 19:46:33 -0400

    Refactoring - Backend Configs - Configure @RestController

--- 02-backend/spring-boot-ecomerce/src/main/java/com/freudromero/ecomerce/controller/CheckoutController.java
+++ 02-backend/spring-boot-ecomerce/src/main/java/com/freudromero/ecomerce/controller/CheckoutController.java
@@ -6,7 +6,6 @@ import com.freudromero.ecomerce.service.CheckoutService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {

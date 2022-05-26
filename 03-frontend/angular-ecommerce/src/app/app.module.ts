b640c19e73eafcb89d548e083a263c33f6962e73
fred <abnertorres58@gmail.com>
2022-05-25 22:22:12 -0400

    Security - VIP Member Access - Protected Routes

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -22,15 +22,16 @@ import { OktaAuth } from '@okta/okta-auth-js';
 import myAppConfig from "./config/my-app-config";
 
 import {
-  OKTA_CONFIG,
+  OKTA_CONFIG, OktaAuthGuard,
   OktaAuthModule,
   OktaCallbackComponent
 } from '@okta/okta-angular';
 
 import { LoginStatusComponent } from './components/login-status/login-status.component';
+import { MembersPageComponent } from './components/members-page/members-page.component';
 
 const oktaConfig = Object.assign({
-  onAuthRequired: (injector) => {
+  onAuthRequired: (oktaAuth, injector) => {
     const router = injector.get(Router);
 
     // Redirect the user to your custom login page
@@ -41,6 +42,8 @@ const oktaConfig = Object.assign({
 const oktaAuth = new OktaAuth(oktaConfig);
 
 const routes: Routes = [
+  {path: 'members', component: MembersPageComponent, canActivate: [ OktaAuthGuard ]},
+
   {path: 'login/callback', component: OktaCallbackComponent},
   {path: 'login', component: LoginComponent},
 
@@ -67,7 +70,8 @@ const routes: Routes = [
     CartDetailsComponent,
     CheckoutComponent,
     LoginComponent,
-    LoginStatusComponent
+    LoginStatusComponent,
+    MembersPageComponent
   ],
   imports: [
     RouterModule.forRoot(routes),

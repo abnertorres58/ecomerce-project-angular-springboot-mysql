32af09cb77ebfcf630c33fcfffda7098ebc344b8
fred <abnertorres58@gmail.com>
2022-05-27 16:16:27 -0400

    Security - Order History - Secure Backend - Protect Endpoints

---
+++ 02-backend/spring-boot-ecomerce/src/main/java/com/freudromero/ecomerce/config/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package com.freudromero.ecomerce.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        // Protect endpoint /api/orders
+        http.authorizeRequests()
+                .antMatchers("/api/orders/**")
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+
+        // Add CORS filters
+        http.cors();
+
+    }
+}

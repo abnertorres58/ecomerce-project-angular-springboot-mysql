18740608081fc21faed658ce1a2422cb33620ab3
fred <abnertorres58@gmail.com>
2022-05-27 16:41:44 -0400

    Security - Order History - Secure Frontend - Create an Interceptor

--- 02-backend/spring-boot-ecomerce/src/main/java/com/freudromero/ecomerce/config/SecurityConfiguration.java
+++ 02-backend/spring-boot-ecomerce/src/main/java/com/freudromero/ecomerce/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.freudromero.ecomerce.config;
 
+import com.okta.spring.boot.oauth.Okta;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,6 +20,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         // Add CORS filters
         http.cors();
+
+        // Force a non-empty response body for 401's to make response more friendly
+        Okta.configureResourceServer401ResponseBody(http);
 
     }
 }

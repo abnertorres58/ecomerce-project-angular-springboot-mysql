e9dd0cb3461269d7e55b09e2b442dbcb4515719c
fred <abnertorres58@gmail.com>
2022-05-27 15:38:50 -0400

    Order History - Frontend Coding - Pre populate Email in the Checkout Form

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -32,6 +32,8 @@ export class CheckoutComponent implements OnInit {
   shippingAddressStates: State[] = [];
   billingAddressStates: State[] = [];
 
+  storage: Storage = sessionStorage;
+
   constructor(private formBuilder: FormBuilder,
               private shopFormService: ShopFormService,
               private cartService: CartService,
@@ -42,6 +44,9 @@ export class CheckoutComponent implements OnInit {
 
     this.reviewCartDetails();
 
+    // Read the user's email address from browser storage
+    const theEmail = JSON.parse(this.storage.getItem('userEmail'));
+
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
         firstName: new FormControl('', [ Validators.required,
@@ -51,7 +56,7 @@ export class CheckoutComponent implements OnInit {
         lastName: new FormControl('', [ Validators.required,
                                                               Validators.minLength(2),
                                                               ShopValidators.notOnlyWhiteSpace]),
-        email: new FormControl('',
+        email: new FormControl(theEmail,
           [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
       shippingAddress: this.formBuilder.group({

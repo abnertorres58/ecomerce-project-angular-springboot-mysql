5951fb0e818b894b63654c48c533325d26fa489b
fred <abnertorres58@gmail.com>
2022-05-30 16:44:36 -0400

    Angular Environment Configuration - Add New Environment

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -33,6 +33,8 @@ export class ProductService {
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
                     +   `&page=${thePage}&size=${thePageSize}`;
 
+    console.log(`Getting products from - ${searchUrl}`);
+
     return this.httpClient.get<GetResponseProducts>(searchUrl);
   }
 

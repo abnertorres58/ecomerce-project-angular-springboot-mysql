4e0526dd88e86128123c4503a96290b4d516bfce
fred <abnertorres58@gmail.com>
2022-05-30 23:16:37 -0400

    Payment Processing - Stripe - Backend Coding - Checkout Service and Component

--- 02-backend/spring-boot-ecomerce/src/main/java/com/freudromero/ecomerce/controller/CheckoutController.java
+++ 02-backend/spring-boot-ecomerce/src/main/java/com/freudromero/ecomerce/controller/CheckoutController.java
@@ -1,9 +1,14 @@
 package com.freudromero.ecomerce.controller;
 
+import com.freudromero.ecomerce.dto.PaymentInfo;
 import com.freudromero.ecomerce.dto.Purchase;
 import com.freudromero.ecomerce.dto.PurchaseResponse;
 import com.freudromero.ecomerce.service.CheckoutService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,5 +28,15 @@ public class CheckoutController {
         PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
 
         return purchaseResponse;
+    }
+
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfo paymentInfo) throws StripeException {
+
+        PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
+
+        String paymentStr = paymentIntent.toJson();
+
+        return new ResponseEntity<>(paymentStr, HttpStatus.OK);
     }
 }

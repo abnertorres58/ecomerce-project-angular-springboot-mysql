f79f2a830d6ba31c230ea94982a4e12d630e2944
zurcacielos <fabianse@gmail.com>
2022-06-24 01:35:03 -0400

    position login widget

--- 03-frontend/angular-ecommerce/src/app/components/login/login.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/login/login.component.ts
@@ -23,7 +23,7 @@ export class LoginComponent implements OnInit {
     // @ts-ignore
     this.oktaSignin = new OktaSignIn({
 
-      logo: 'assets/images/logo.png',
+      // logo: 'assets/images/logo.png',
       baseUrl: myAppConfig.oidc.issuer.split('/oauth2')[0],
       clientId: myAppConfig.oidc.clientId,
       redirectUri: myAppConfig.oidc.redirectUri,

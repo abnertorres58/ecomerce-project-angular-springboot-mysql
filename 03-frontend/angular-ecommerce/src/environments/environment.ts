5f3c6aed246e72cb1f9714c4186ac7a70bae9513
fred <abnertorres58@gmail.com>
2022-06-24 23:55:51 -0400

    LogIn and Credit card warnings in place

--- 03-frontend/angular-ecommerce/src/environments/environment.ts
+++ 03-frontend/angular-ecommerce/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
 
   // Add the custom environment config
-  fredsshopApiUrl: "http://localhost:5000/api",
+  fredsshopApiUrl: "http://freudsshop.us-east-2.elasticbeanstalk.com/api",
   stripePublishableKey: "pk_test_51L5IuWANMeQDotHMyiGAWDvGn1NlHrMMG7BFN4p53pCSfv89Quxjbj51NdlkwCEAxXfQdCssfnsQGmmuZqcSB0Ya00DMWs8wYz"
 
 };

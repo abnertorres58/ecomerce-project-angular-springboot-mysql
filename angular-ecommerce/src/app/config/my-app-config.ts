b3e6901f7c66884e1f48401ced878c2641e6619b
fred <abnertorres58@gmail.com>
2022-06-30 21:40:34 -0400

    change folders

---
+++ angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,12 @@
+export default {
+
+  oidc: {
+    clientId: '0oa54mwi8xcsnqfMk5d7',
+    issuer: 'https://dev-74646655.okta.com/oauth2/default',
+    // redirectUri: 'https://localhost:4200/login/callback',
+    //redirectUri: 'http://localhost:8443/login/callback', // pointing towards port 8443
+    redirectUri: 'http://freudsshop.us-east-2.elasticbeanstalk.com/login/callback', // pointing towards aws
+    scopes: ['openid', 'profile', 'email']
+  }
+
+}
